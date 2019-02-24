ac3de044bfda0cdbe562d87edf6c1dd61a123411
yodfhafx <yodfha2014@gmail.com>
2019-02-24 15:33:51 +0700

    connect mysql

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'node-mysql-crud',
+  debug: false
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Connect database
+router.get('/testdb', (req, res, next) => {
+  if(db != null) {
+    res.send('Connection success');
+  } else {
+    res.send('Connection failed');
+  }
 });
 
 module.exports = router;
